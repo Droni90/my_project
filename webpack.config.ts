49d938e9fb2c69b0456bc6bfb3bfeb0db3842b78
anri <a.botygin@brusnika.ru>
2022-09-13 11:17:10 +0300

    set dev server, add env

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,22 +2,25 @@ import path from "path";
 import webpack from "webpack";
 
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
-import { BuildPaths } from "./config/build/types/config";
+import { BuildEnv, BuildMode, BuildPaths } from "./config/build/types/config";
 
-const paths: BuildPaths = {
-  entry: path.resolve(__dirname, "src", "index.ts"),
-  html: path.resolve(__dirname, "public", "index.html"),
-  build: path.resolve(__dirname, "build"),
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    html: path.resolve(__dirname, "public", "index.html"),
+    build: path.resolve(__dirname, "build"),
+  };
+
+  const mode: BuildMode = env.mode || "development";
+
+  const isDev: boolean = mode === "development";
+  const PORT: number = env.port || 3005;
+
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+  return config;
 };
-
-const mode: string = "development";
-
-const isDev: boolean = mode === "development";
-
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: "development",
-  paths,
-  isDev,
-});
-
-export default config;
