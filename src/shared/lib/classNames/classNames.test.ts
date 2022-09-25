327d58b4d6132d3804e05ea904944dbdeaae2380
Anri Rey <dron-i90@yandex.ru>
2022-09-25 21:48:11 +0300

    add jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,22 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+  test("with only 1st param", () => {
+    expect(classNames("someClass")).toBe("someClass");
+  });
+
+  test("with additional class", () => {
+    const expected: string = "someClass class1 class2";
+    expect(classNames("someClass", ["class1", "class2"])).toBe(expected);
+  });
+
+  test("with mods", () => {
+    const expected: string = "someClass class1 class2 hovered scrollable";
+    expect(
+      classNames("someClass", ["class1", "class2"], {
+        hovered: true,
+        scrollable: true,
+      })
+    ).toBe(expected);
+  });
+});
