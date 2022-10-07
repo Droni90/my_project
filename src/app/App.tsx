51576dcc0167ee188b1ee713e45dd43748d6caf3
anri <a.botygin@brusnika.ru>
2022-10-07 18:50:35 +0300

    lint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,8 @@ import { UseTheme } from 'app/providers/ThemeProvider/lib/UseTheme';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
 import { AppRouter } from './providers/router';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
     <div className={classNames('app', [theme])}>
       <Suspense fallback="">
         <Navbar />
+        <Modal />
         <div className="content-page">
           <Sidebar />
           <AppRouter />
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,19 @@
+import { Children, FC, ReactNode } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+  className?: string;
+  children?: ReactNode;
+}
+
+export const Modal: FC<ModalProps> = (props) => {
+  const { className, children } = props;
+  return (
+    <div className={classNames(cls.Modal, [className])}>
+      <div className={cls.overlay}>
+        <div className={cls.content}>{children}</div>
+      </div>
+    </div>
+  );
+};
