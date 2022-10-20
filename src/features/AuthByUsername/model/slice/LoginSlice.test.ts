653574c295ba9ad66d14ef67c90bd47e1aa9a022
anri <a.botygin@brusnika.ru>
2022-10-20 20:44:17 +0300

    add test for loginbyUsername

---
+++ src/features/AuthByUsername/model/slice/LoginSlice.test.ts
@@ -0,0 +1,23 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/LoginShema';
+import { loginActions, loginReducer } from './LoginSlice';
+
+describe('loginSlice.test', () => {
+  test('test set username', async () => {
+    const state: DeepPartial<LoginSchema> = {
+      username: '123',
+    };
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setUsername('456'))
+    ).toEqual({ username: '456' });
+  });
+
+  test('test set password', async () => {
+    const state: DeepPartial<LoginSchema> = {
+      password: '123',
+    };
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setPassword('456'))
+    ).toEqual({ password: '456' });
+  });
+});
