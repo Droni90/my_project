23216ef7909e7e095231e0bb72de3890fe6d78ec
anri <a.botygin@brusnika.ru>
2022-10-20 21:20:25 +0300

    navbar darkj

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -1,8 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Theme } from 'app/providers/ThemeProvider';
 import { StoreDecorator } from 'shared/config/storybook/storeDecorator/storeDecorator';
-import { ThemeDecorator } from 'shared/config/storybook/themeDecorator/themeDecorator';
 import { Navbar } from './Navbar';
 
 export default {
@@ -18,10 +16,6 @@ const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 export const Light = Template.bind({});
 Light.args = {};
 Light.decorators = [StoreDecorator({})];
-
-export const Dark = Template.bind({});
-Dark.args = {};
-Dark.decorators = [StoreDecorator({})];
 
 export const AuthNavbar = Template.bind({});
 AuthNavbar.args = {};
