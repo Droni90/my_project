9fdf1bff88d529077590e6a274795ef0e8a8793a
anri <a.botygin@brusnika.ru>
2022-11-18 18:05:07 +0300

    before deploy

--- webpack.config.ts
+++ webpack.config.ts
@@ -12,8 +12,10 @@ export default (env: BuildEnv) => {
   const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     html: path.resolve(__dirname, 'public', 'index.html'),
-    build: path.resolve(__dirname, 'configBuild'),
+    build: path.resolve(__dirname, 'build'),
     src: path.resolve(__dirname, 'src'),
+    locales: path.resolve(__dirname, 'public', 'locales'),
+    buildLocales: path.resolve(__dirname, 'build', 'locales'),
   };
 
   const mode: BuildMode = env.mode || 'development';
